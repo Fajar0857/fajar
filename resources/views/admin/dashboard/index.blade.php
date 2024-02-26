287001c08476d247d18c564e137a5241c96c42ca
fajar <fajar55@gmail.com>
2024-02-26 13:31:52 +0700

    web gallery

---
+++ resources/views/admin/dashboard/index.blade.php
@@ -0,0 +1,8 @@
+@extends('admin.layout')
+@section('content')
+    <div class="card bg-primary text-light">
+        <div class="card-body">
+            <h1>Assalammu'alaikum</h1>
+        </div>
+    </div>
+@endsection
